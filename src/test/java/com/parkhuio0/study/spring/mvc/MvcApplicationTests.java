1c125265e5bf725bc162680ad51b1ddb0972eb29
parkhuiwo0 <huiwo0@naver.com>
2021-04-19 22:13:54 +0900

    initialize project

---
+++ src/test/java/com/parkhuio0/study/spring/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhuio0.study.spring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
