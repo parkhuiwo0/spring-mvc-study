2c9f2dd1852852dc9df56f66045195527a90a3ff
parkhuiwo0 <huiwo0@naver.com>
2021-04-19 22:44:55 +0900

    HelloServlet.java 추가 (Servlet 등록)

---
+++ src/main/java/com/parkhuio0/study/spring/mvc/basic/HelloServlet.java
@@ -0,0 +1,32 @@
+package com.parkhuio0.study.spring.mvc.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    // 서블릿이 호출되면, 이 서블릿 함수가 호출된다.
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        super.service(req, resp);
+        System.out.println("HelloServlet.service");
+        /*
+        서블릿 요청이 오면 WAS가 Request, Response 객체를 만들어서 던져준다.
+        그래서, 웹 브라우저가 HTTP 요청 메시지를 만들어서 던진 것이다. (request)
+         */
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
