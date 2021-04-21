03a3499b8223afa5248ef6b1e02163d452a862ac
parkhuiwo0 <huiwo0@naver.com>
2021-04-21 21:23:30 +0900

    HTTP 요청 데이터 (API 메시지 바디 (JSON))

---
+++ src/main/java/com/parkhuio0/study/spring/mvc/basic/HelloData.java
@@ -0,0 +1,12 @@
+package com.parkhuio0.study.spring.mvc.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
