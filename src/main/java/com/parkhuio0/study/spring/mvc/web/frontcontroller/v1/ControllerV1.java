04c50bda1a5154c6f85076080153fc92cd1534a1
휴/박희우 <parkhuiwo0@gmail.com>
2021-04-26 15:17:02 +0900

    MVC 프레임 워크 만들기 - 프론트 컨트롤러를 위한 ControllerV1 인터페이스 추가

---
+++ src/main/java/com/parkhuio0/study/spring/mvc/web/frontcontroller/v1/ControllerV1.java
@@ -0,0 +1,11 @@
+package com.parkhuio0.study.spring.mvc.web.frontcontroller.v1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV1 {
+
+    void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
