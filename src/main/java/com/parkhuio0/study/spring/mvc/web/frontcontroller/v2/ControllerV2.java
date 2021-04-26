ca6c4b0636a77af1d0fa095e118d6e5ca01447aa
휴/박희우 <parkhuiwo0@gmail.com>
2021-04-26 17:45:47 +0900

    MVC 프레임 워크 만들기 - View 분리를 위한 ControllerV2 추가

---
+++ src/main/java/com/parkhuio0/study/spring/mvc/web/frontcontroller/v2/ControllerV2.java
@@ -0,0 +1,13 @@
+package com.parkhuio0.study.spring.mvc.web.frontcontroller.v2;
+
+import com.parkhuio0.study.spring.mvc.web.frontcontroller.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV2 {
+
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
