c1429d5c25c3c91f3039571e9423727d8f8c49dc
휴/박희우 <parkhuiwo0@gmail.com>
2021-04-26 17:50:31 +0900

    MVC 프레임 워크 만들기 - View 분리한 ControllerV2 추가

---
+++ src/main/java/com/parkhuio0/study/spring/mvc/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,17 @@
+package com.parkhuio0.study.spring.mvc.web.frontcontroller.v2.controller;
+
+import com.parkhuio0.study.spring.mvc.web.frontcontroller.MyView;
+import com.parkhuio0.study.spring.mvc.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
---
+++ src/main/java/com/parkhuio0/study/spring/mvc/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,25 @@
+package com.parkhuio0.study.spring.mvc.web.frontcontroller.v2.controller;
+
+import com.parkhuio0.study.spring.mvc.domain.member.Member;
+import com.parkhuio0.study.spring.mvc.domain.member.MemberRepository;
+import com.parkhuio0.study.spring.mvc.web.frontcontroller.MyView;
+import com.parkhuio0.study.spring.mvc.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
